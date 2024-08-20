299283be056448a4c9e8f903c1fe31cb18fad99c
cbroschinski <broschinski@uni-bielefeld.de>
2024-08-20 14:28:16 +0000

    add update script

---
+++ update_treemaps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Update script for openapc-treemaps, should be run after changes to the OLAP server have been made (update_olap.sh)
+# This script should be executed from ~/dev
+
+# Delete old yamls, copy static files
+if [ -d  ~/dev/openapc-treemaps/sites ]
+	then
+		rm -rf ~/dev/openapc-treemaps/sites
+fi
+mkdir ~/dev/openapc-treemaps/sites
+cp ~/dev/openapc-treemaps/static_sites/*.yaml ~/dev/openapc-treemaps/sites/
+# Pull a fresh copy of the OpenAPC core data file and use it to generate the yaml files
+cd ~/dev/openapc-de
+git pull
+cd ~/dev/openapc-olap
+. venv/bin/activate
+python assets_generator.py -d ~/dev/openapc-treemaps/sites yamls
+deactivate
+# Delete old builds
+cd ~/dev/openapc-treemaps
+if [ -d build ]
+	then
+		rm -rf build
+fi
+# Rebuild the site and copy it
+. venv/bin/activate
+python freeze.py
+sudo cp -r build/* /var/www/openapc-treemaps
